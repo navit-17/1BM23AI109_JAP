13ed5cce29e0ac982e2d048812e45fe60afd63a9
navit-17 <navitmathur.ai23@bmsce.ac.in>
2025-03-12 15:15:51 +0530

    Add files via upload

---
+++ gym_membership.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class gym_membership {
+    public static void main(String[] args){
+        details();
+    }
+
+    public static int calc(int dur){
+        return (dur * 1200);
+    }
+    public static void details(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your Name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        System.out.print("Enter your membership status (T/F): ");
+        boolean mem_stat = sc.nextBoolean();
+        System.out.print("Enter your membership duration (m):  ");
+        int duration = sc.nextInt();
+        int fee = calc(duration);
+        System.out.println("Your Fees for " + duration + "months is " + fee);
+    }
+}

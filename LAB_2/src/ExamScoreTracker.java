0a053075f176c59b6da342073e7009f2f86fd341
navit <navitmathur.ai23@bmsce.ac.in>
2025-05-20 21:39:51 +0530

    " "

---
+++ LAB_2/src/ExamScoreTracker.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ExamScoreTracker {
+    public static void main(String[] args){
+        final int NUM_STUDENTS = 5;
+        int[] examScores = new int[NUM_STUDENTS];
+
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
---
+++ LAB_3/src/string_buffer.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class string_buffer {
+    public static void main(String[] args) {
+    StringBuffer sb1 = new StringBuffer();
+    System.out.println("Capacity: " + sb1.capacity());
+
+    StringBuffer sb2 = new StringBuffer("Hello");
+    System.out.println("Capacity: " + sb2.capacity());
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a string: ");
+    String input = sc.nextLine();
+    StringBuffer sb3 = new StringBuffer(input);
+    sb3.reverse();
+    String reversedUc = sb3.toString().toUpperCase();
+    System.out.print("reversed and uppercase string: " + reversedUc);
+
+    System.out.print("Enter a string to append: ");
+    String appendString = sc.nextLine();
+    sb3.append(appendString);
+    System.out.println("String after appending: " + sb3);
+
+    }
+}
